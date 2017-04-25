6283e9f833678f8b25a85ce8f8722d6ec2701349
PLEASEWORK\Krzysztof <krzysztof.murczynski@gmail.com>
2017-04-25 16:39:57 +0200

    Changed many names from lvl1 to lvl2, few sprites, empty animations for lvl1

---
+++ Assets/Scripts/GM_Multilevel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GM_Multilevel : MonoBehaviour {
+
+    public GameObject[] buttonsOfObjects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadSceneById(int id)
+    {
+        SceneManager.LoadScene(id);
+    }
+
+    public void OnButtonPress()
+    {
+        GameObject.Find("Scene").GetComponent<Animator>().SetTrigger("NewStage");
+    }
+
+    void ActiveButtons(bool active)
+    {
+        if(active == true)
+        {
+            foreach (GameObject button in buttonsOfObjects)
+            {
+                button.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (GameObject button in buttonsOfObjects)
+            {
+                button.SetActive(false);
+            }
+        }
+    }
+    
+
+    private void SetLevelsOfObject()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void YouWin()
+    {
+
+    }
+}
